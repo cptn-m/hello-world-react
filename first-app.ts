9f72b03a3cbd2d21a67419b14cb3fcf95e759467
cptn-m <mhu08@outlook.com>
2024-05-07 23:08:50 +1000

    create custom functions

--- first-app.ts
+++ first-app.ts
@@ -46,3 +46,34 @@ var_strict_object = {
 
 let var_array_str: Array<string>; // needs the related value types
 let var_alt_array_str: string[] // this is the shortcut and more commonly used
+
+
+// example of a complex function: function add (a:number, b:{value: number})
+
+// the function "add" takes two inputs and and b with the specified types to yield an expected output
+function my_add (a:number, b:number) :any {
+    const result = a + b;
+    console.log(result);
+}
+
+// this is a function which will return no results
+function my_add_void (a:number, b:number) : void {
+    const result = a + b;
+    console.log(result);
+}
+
+
+// lets create an alias type / create custom types
+type addFN = (a:number, b:number) => number; 
+
+function my_calc(
+    a:number, 
+    b:number, 
+    // rather than stroring this janky code: calcFn: (a: number, b: number) => number 
+    // we can use this instead
+    calcFn: addFN // lets use the type "addFN"
+    ) {
+    calcFn(a,b)
+}
+
+my_calc(2,5,my_add)
