b8247add34d4c2fd4cba77baa7fadec8a261c9d9
cptn-m <mhu08@outlook.com>
2024-05-07 22:40:50 +1000

    file with examples of setting up variables with inference and explicit types

---
+++ first-app.ts
@@ -0,0 +1,12 @@
+let userName = 'Max'; // variable with inference type safety 
+let var_str: string;  // variable with explicit typescript 
+let var_any; // variable without type safety
+let var_overkill: string = 'Max'; // this is overkill
+let var_boolean = true;
+let var_num = 11;
+// string, number, boolean 
+
+// type save examples
+var_str = 'this is a string'
+var_boolean = true
+var_num = 12 
