316c485041ffbba6328d877eba87366c1ab6cec4
cptn-m <mhu08@outlook.com>
2024-05-27 14:54:43 +1000

    building a wrapper component that renders different elements

--- hello-world-adv/src/App.tsx
+++ hello-world-adv/src/App.tsx
@@ -1,10 +1,13 @@
 import Input from './components/Input.tsx';
+import Button from './components/Button.tsx';
 
 function App() {
   return (
     <main>
       <Input id="name" label="Your Name" type="text"/>
       <Input id="age" label="Your Age" type="number"/>
+      <Button el="button"href=""> A Button</Button>
+      <Button el="anchor"href='https://google.com'> A Link</Button>
     </main>
   )
 }
